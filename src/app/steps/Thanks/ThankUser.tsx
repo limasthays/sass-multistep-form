567d2be40f2fe06df99890535a093a7fb9f914af
User <limasthays@gmail.com>
2023-07-08 19:23:51 -0300

    thank user component

--- src/app/steps/Thanks/ThankUser.tsx
+++ src/app/steps/Thanks/ThankUser.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
+import ThankYouIcon from '../../assets/images/icon-thank-you.svg'
+
 function ThankUser() {
 	return (
-		<section>
-			<h2>Thank you!</h2>
-		</section>
+		<>
+			<div>
+				<Image src={ThankYouIcon} alt="Checkmark icon" />
+
+				<h2>Thank you!</h2>
+
+				<p>
+					Thanks for confirming your subscription! We hope you have fun using
+					our platform. If you ever need support, please feel free to email us
+					at support@loremgaming.com.
+				</p>
+			</div>
+		</>
 	)
 }
 
