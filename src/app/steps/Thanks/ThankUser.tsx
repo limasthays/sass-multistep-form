e09064b4671591ad289cfe62edb77d18bcc7e2cd
User <limasthays@gmail.com>
2023-07-10 18:44:39 -0300

    updates finish form component

--- src/app/steps/Thanks/ThankUser.tsx
+++ src/app/steps/Thanks/ThankUser.tsx
@@ -1,19 +1,22 @@
 import Image from 'next/image'
 import ThankYouIcon from '../../assets/images/icon-thank-you.svg'
+import './style.scss'
 
 function ThankUser() {
 	return (
 		<>
-			<div>
-				<Image src={ThankYouIcon} alt="Checkmark icon" />
+			<div className="finish-form">
+				<Image src={ThankYouIcon} alt="Checkmark icon" width="56" />
 
-				<h2>Thank you!</h2>
+				<div>
+					<h2>Thank you!</h2>
 
-				<p>
-					Thanks for confirming your subscription! We hope you have fun using
-					our platform. If you ever need support, please feel free to email us
-					at support@loremgaming.com.
-				</p>
+					<p>
+						Thanks for confirming your subscription! We hope you have fun using
+						our platform. If you ever need support, please feel free to email us
+						at support@loremgaming.com.
+					</p>
+				</div>
 			</div>
 		</>
 	)
